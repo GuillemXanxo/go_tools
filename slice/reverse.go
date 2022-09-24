42205b906feb344e54155d3c8137f0ac382fff29
guillem-xanxo <guillem.xanxo@volcanicinternet.com>
2022-09-24 20:16:44 +0200

    reverse.go

---
+++ slice/reverse.go
@@ -0,0 +1,19 @@
+package slice
+
+// Reverse returns a new copy of the slice with the elements ordered in reverse.
+// This is useful when combined with Sort to get a descending sort order:
+//
+//   ss.Sort().Reverse()
+//
+func Reverse[T any](ss []T) []T {
+	if len(ss) < 2 {
+		return ss
+	}
+
+	sorted := make([]T, len(ss))
+	for i := 0; i < len(ss); i++ {
+		sorted[i] = ss[len(ss)-i-1]
+	}
+
+	return sorted
+}
